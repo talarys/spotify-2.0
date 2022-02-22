e372a8ac079f7448d83686fdbd7665a94c8b8ad7
visotoniki9 <d.simon.ssx0@gmail.com>
2022-02-22 19:45:45 +0100

    Added sidebar

---
+++ components/Sidebar.jsx
@@ -0,0 +1,52 @@
+import {
+  HomeIcon,
+  SearchIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+  RssIcon,
+  HeartIcon,
+} from '@heroicons/react/outline';
+
+import tw from 'tailwind-styled-components';
+
+function Sidebar() {
+  return (
+    <div className="text-gray-500 p-5 border-r text-sm border-gray-900">
+      <div className="space-y-4">
+        <Button>
+          <HomeIcon className="h-5 w-5" />
+          <p>Home</p>
+        </Button>
+        <Button>
+          <SearchIcon className="h-5 w-5" />
+          <p>Search</p>
+        </Button>
+        <Button>
+          <LibraryIcon className="h-5 w-5" />
+          <p>Library</p>
+        </Button>
+        <hr className="border-t-[0.1px] border-gray-900" />
+        <Button>
+          <PlusCircleIcon className="h-5 w-5" />
+          <p>Create Playlist</p>
+        </Button>
+        <Button>
+          <HeartIcon className="h-5 w-5" />
+          <p>Liked Songs</p>
+        </Button>
+        <Button>
+          <RssIcon className="h-5 w-5" />
+          <p>Your episodes</p>
+        </Button>
+        <hr className="border-t-[0.1px] border-gray-900" />
+        {/* {Playlists...} */}
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
+
+const Button = tw.button`
+flex items-center space-x-2 hover:text-white
+`;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,21 @@
+import Head from 'next/head';
+import Sidebar from '../components/Sidebar';
+
 export default function Home() {
   return (
-    <div>hello world</div>
+    <div>
+      <Head>
+        <title>Spotify 2.0</title>
+      </Head>
+      <div>
+        <main>
+          <Sidebar />
+          {/* Center */}
+        </main>
+        <div>
+          {/* Player */}
+        </div>
+      </div>
+    </div>
   );
 }
