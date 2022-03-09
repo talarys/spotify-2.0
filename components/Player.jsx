4e11be1485123419bb77f6a5daba7218232a81a4
visotoniki9 <d.simon.ssx0@gmail.com>
2022-03-09 20:24:55 +0100

    Finished player component

--- components/Player.jsx
+++ components/Player.jsx
@@ -1,6 +1,7 @@
 import { useSession } from 'next-auth/react';
 import { useRecoilState } from 'recoil';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 import {
   SwitchHorizontalIcon,
   RefreshIcon,
@@ -14,6 +15,7 @@ import {
   PauseIcon,
 } from '@heroicons/react/solid';
 
+import { debounce } from 'lodash';
 import useSpotify from '../hooks/useSpotify';
 import useSongInfo from '../hooks/useSongInfo';
 import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
@@ -21,11 +23,24 @@ import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
 function Player() {
   const songInfo = useSongInfo();
   const spotifyApi = useSpotify();
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
   const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
   const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
-  const [volume, setVolume] = useState();
+  const [volume, setVolume] = useState(50);
   const [shuffle, setShuffle] = useState(false);
+  const [repeat, setRepeat] = useState(false);
+
+  const errorHandler = (err) => {
+    spotifyApi.getMyDevices()
+      .then((data) => {
+        if (!data.body.devices.length) {
+          toast.error('No devices found');
+        } else {
+          console.error(err);
+        }
+      })
+      .catch((error) => console.error(error));
+  };
 
   useEffect(() => {
     if (spotifyApi.getAccessToken() && !currentTrackId) {
@@ -40,29 +55,64 @@ function Player() {
     }
   }, [currentTrackId, spotifyApi, session]);
 
-  const nextSong = () => spotifyApi.skipToNext();
-  const prevSong = () => spotifyApi.skipToPrevious();
+  const debouncedAdjustVolume = useCallback(
+    debounce((vol) => {
+      spotifyApi.setVolume(vol)
+        .catch((err) => errorHandler(err));
+    }, 500),
+  );
+
+  useEffect(() => {
+    if (volume >= 0 && volume <= 100) {
+      debouncedAdjustVolume(volume);
+    }
+  }, [volume]);
+
+  const nextSong = () => spotifyApi.skipToNext()
+    .catch((err) => errorHandler(err));
+
+  const prevSong = () => spotifyApi.skipToPrevious()
+    .catch((err) => errorHandler(err));
+
   const toggleShuffle = () => {
     if (shuffle) {
-      spotifyApi.setShuffle(false);
-      setShuffle(false);
+      spotifyApi.setShuffle(false)
+        .then(setShuffle(false))
+        .catch((err) => errorHandler(err));
     } else {
-      spotifyApi.setShuffle(true);
-      setShuffle(true);
+      spotifyApi.setShuffle(true)
+        .then(setShuffle(true))
+        .catch((err) => errorHandler(err));
     }
   };
+
+  const toggleRepeat = () => {
+    if (repeat) {
+      spotifyApi.setRepeat('off')
+        .then(setRepeat(false))
+        .catch((err) => errorHandler(err));
+    } else {
+      spotifyApi.setRepeat('context')
+        .then(setRepeat(true))
+        .catch((err) => errorHandler(err));
+    }
+  };
+
   const handlePlayPause = () => {
     if (isPlaying) {
-      spotifyApi.pause();
-      setIsPlaying(false);
+      spotifyApi.pause()
+        .then(setIsPlaying(false))
+        .catch((err) => errorHandler(err));
     } else {
-      spotifyApi.play();
-      setIsPlaying(true);
+      spotifyApi.play()
+        .then(setIsPlaying(true))
+        .catch((err) => errorHandler(err));
     }
   };
 
   return (
-    <div className="p-4 grid grid-cols-3 bg-[#181818] text-gray-300 w-screen absolute bottom-0">
+    <div className="space-y-2 p-4 grid sm:grid-cols-2 md:grid-cols-3 bg-[#181818] text-gray-300 w-screen absolute bottom-0">
+
       {/* Left */}
       <div className="flex items-center space-x-4">
         <img className="h-20 w-20 ml-5 p" src={songInfo?.album?.images[0]?.url} />
@@ -73,28 +123,46 @@ function Player() {
           <p>{songInfo?.artists?.[0]?.name}</p>
         </div>
       </div>
+
       {/* Center */}
       <div className="flex items-center justify-center space-x-2">
-        <SwitchHorizontalIcon onClick={toggleShuffle} className={`button ${shuffle && 'text-green-400'}`} />
-        <RewindIcon onClick={prevSong} className="button" />
+        <SwitchHorizontalIcon
+          onClick={toggleShuffle}
+          className={`button ${shuffle && 'text-green-400'}`}
+        />
+        <RewindIcon
+          onClick={prevSong}
+          className="button"
+        />
         {isPlaying ? (
-          <PauseIcon onClick={handlePlayPause} className="button w-14 h-14" />
+          <PauseIcon
+            onClick={handlePlayPause}
+            className="button w-14 h-14"
+          />
         ) : (
-          <PlayIcon onClick={handlePlayPause} className="button w-14 h-14" />
+          <PlayIcon
+            onClick={handlePlayPause}
+            className="button w-14 h-14"
+          />
         )}
         <FastForwardIcon onClick={nextSong} className="button" />
-        <RefreshIcon className="button" />
+        <RefreshIcon
+          onClick={toggleRepeat}
+          className={`button ${repeat && 'text-green-400'}`}
+        />
       </div>
+
       {/* Right */}
-      <div className="flex items-center justify-center space-x-3">
-        <VolumeOffIcon className="button" />
+      <div className="hidden md:flex items-center justify-end space-x-3">
+        <VolumeOffIcon onClick={() => setVolume(0)} className="button" />
         <input
           type="range"
           min={0}
           max={100}
-          defaultValue={50}
+          value={volume}
+          onChange={(e) => setVolume(Number(e.target.value))}
         />
-        <VolumeUpIcon className="button" />
+        <VolumeUpIcon onClick={() => setVolume(100)} className="button" />
       </div>
     </div>
   );
