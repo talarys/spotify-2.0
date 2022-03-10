83077009f63559797a6809301d844cd874c2a042
visotoniki9 <d.simon.ssx0@gmail.com>
2022-03-10 15:04:32 +0100

    Fixing prod bug

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,9 +3,10 @@ import { NextResponse } from 'next/server';
 
 export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
+
   const { pathname } = req.nextUrl;
 
-  if (pathname.includes('/api/auth') || pathname.includes('.svg') || token) {
+  if (pathname.includes('/api/auth') || token) {
     return NextResponse.next();
   }
 
